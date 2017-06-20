7fa9cd2d9421f6a6700a6d3c23a94368d8a913d3
nurungji <nick.park1860@gmail.com>
2017-06-20 22:39:41 +0900

    Test unit test and functional test

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -1,6 +1,18 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class FunctionalTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_before_starting_project(self):
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('test', self.browser.title)
+        self.fail('Finish the test!')
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
